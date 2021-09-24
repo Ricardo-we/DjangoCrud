f7c7d2953bcef554484ca930cf80d5f361fd0abc
Ricardo-we <totigamer75@gmail.com>
2021-09-23 18:11:36 -0600

    Last commit

--- Django_crud/settings.py
+++ Django_crud/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -113,6 +114,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+django_heroku.settings(locals())
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate
-web: gunicorn ChatOnline.wsgi --log-file -
+web: gunicorn Django_crud.wsgi --log-file -
